77da6f7e5f5f712a72d5e56194b45d3197c21c41
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 01:09:34 +0330

    add basic middlewares

---
+++ back-end/middleware/async.js
@@ -0,0 +1,11 @@
+function asyncMiddleware(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      next(e);
+    }
+  };
+}
+
+module.exports = asyncMiddleware;
