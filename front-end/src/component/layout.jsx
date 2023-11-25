e846a1f2e78f9825f961af7c7c2c846efa9ecb46
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 02:12:19 +0330

    front: add toast

--- front-end/src/component/layout.jsx
+++ front-end/src/component/layout.jsx
@@ -1,9 +1,23 @@
 import Navbar from "./navbar.jsx";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
   return (
     <div className="p-4 min-h-screen flex flex-col">
+      <ToastContainer
+        style={{ top: "1rem" }}
+        hideProgressBar={false}
+        position="top-right"
+        autoClose={3000}
+        limit={2}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Navbar />
       <Outlet />
     </div>
