888cfdd4a8a81f3d3b7e823ce493904c620f4ab5
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 02:11:21 +0330

    back: implement login api

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,28 +1,28 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
 
-router.post("/signup", async (req, res) => {
+router.post("/", async (req, res) => {
   const { body } = req;
-  console.log(body);
-  // const { error } = validate(body);
-  // if (error) return res.status(400).send(error.details[0].message);
-  //
-  // const { email, password } = body;
-  // let user = await User.findOne({ email });
-  // if (!user) return res.status(400).send("invalid email or password");
-  //
-  // const validPsw = await bcrypt.compare(password, user.password);
-  // if (!validPsw) return res.status(400).send("invalid email or password");
-  //
-  // const token = user.generateAuthToken();
-  // res.send(token);
+  const { error } = validate(body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const { email, password } = body;
+  let user = await User.findOne({ email });
+  if (!user) return res.status(400).send("invalid email or password");
+
+  const validPsw = await bcrypt.compare(password, user.password);
+  if (!validPsw) return res.status(400).send("invalid email or password");
+
+  const token = user.generateAuthToken();
+  res.send({ token });
 });
 
 function validate(req) {
   const schema = Joi.object().keys({
-    name: Joi.string().min(3).max(255).required(),
-    username: Joi.string().min(10).max(255).required(),
+    email: Joi.string().email().min(10).max(255).required(),
     password: Joi.string().min(6).max(255).required(),
   });
 
