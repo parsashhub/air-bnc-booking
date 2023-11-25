a158b0ea1458170c033de024497c39658c3f50f6
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 02:11:01 +0330

    back: implement user model.

---
+++ back-end/models/user.js
@@ -0,0 +1,48 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    process.env.JWT_PRIVATE_KEY,
+  );
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = Joi.object().keys({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email().min(8).max(255).required(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports = {
+  User: User,
+  validate: validateUser,
+};
