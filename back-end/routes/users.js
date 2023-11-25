33de82c214cdad4cbc22497eb34f5435dc41ec63
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 02:12:05 +0330

    back: implement user api

---
+++ back-end/routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const _ = require("lodash");
+const { validate, User } = require("../models/user");
+
+router.post("/", async (req, res) => {
+  const { body } = req;
+  const { error } = validate(body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const { name, email, password } = body;
+  let user = await User.findOne({ email });
+  if (user) return res.status(400).send("user already registered");
+
+  user = new User({ name, email, password });
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  res.status(201).send({ data: _.pick(user, ["name", "email", "_id"]) });
+});
+
+module.exports = router;
