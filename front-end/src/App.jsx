1e513bc839c83b1a6d53d2358613e55546d2f7c4
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 16:35:53 +0330

    front: update navbar and implement the profile route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/auth/signUp/index.jsx";
 import axios from "axios";
 import { UserContextProvider } from "./userContext";
 import QueryClientProvider from "./queryClientProvider.jsx";
+import Profile from "./pages/profile/index.jsx";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:3001";
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/auth/login" element={<Login />} />
             <Route path="/auth/signup" element={<SignUp />} />
             <Route path="*" element={<div>404</div>} />
---
+++ front-end/src/pages/profile/index.jsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+    return <div>account page</div>
+};
+
+export default Profile;
