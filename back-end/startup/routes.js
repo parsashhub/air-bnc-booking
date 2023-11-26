964bb82dfb534372fcf769d74d7ee3b560767f5f
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-26 14:16:31 +0330

    back: add cookie parser

--- back-end/startup/routes.js
+++ back-end/startup/routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const error = require("../middleware/error");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const cookieParser = require("cookie-parser")
+
 module.exports = function (app) {
   app.use(express.json());
+  app.use(cookieParser())
   app.use("/api/users", users);
   app.use("/api/auth", auth);
 
