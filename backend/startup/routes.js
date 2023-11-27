124e20f217d6547653dc8975837aae70833831e8
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-27 10:19:05 +0330

    back: add api for upload and pics

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const error = require("../middleware/error");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const upload = require("../routes/upload");
 const cookieParser = require("cookie-parser")
 
 module.exports = function (app) {
@@ -9,6 +10,8 @@ module.exports = function (app) {
   app.use(cookieParser())
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/upload", upload);
+  app.use("/api/pics", express.static("uploads"))
 
   app.use(error);
 };
