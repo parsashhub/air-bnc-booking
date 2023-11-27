700860dcefbf5698be0c9675af932f78c477bbaa
Parsa_Shabanpour <parsash1380@gmail.com>
2023-11-27 10:16:59 +0330

    front: update user context

--- frontend/src/userContext.jsx
+++ frontend/src/userContext.jsx
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { toast } from "react-toastify";
 
 export const UserContext = createContext({});
 
@@ -13,7 +12,7 @@ export const UserContextProvider = ({ children }) => {
     setUser(data);
   }, [data]);
 
-  if (error) toast.error("fetching user failed");
+  if (error) console.error("fetching user failed");
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -26,6 +25,5 @@ export const useUsers = () =>
   useQuery({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users/me").then((res) => res.data?.data),
-    retry: 2,
     staleTime: 60 * 1000 * 60, // 1 hour
   });
